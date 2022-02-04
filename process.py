319201b07efa7eec169147b9094ce862fab91ba8
Lukas Nesittka <97050688+LukasNesittka@users.noreply.github.com>
2022-02-04 09:15:08 -0600

    PythonComplete

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
-log_file = open("um-server-01.txt")
+log_file = open("um-server-01.txt") #Opens the text file to read.
 
 
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
-            print(line)
+def sales_reports(log_file): #This is a function. It takes a file in as a variable
+    for line in log_file: #This loops through every line in our file
+        line = line.rstrip() #This removes any white space at the end or start of our string
+        day = line[0:3] #This line defines that the day is equal to everything from the start of the line until 3 characters in.
+        if day == "Mon": #This statement is truthy when the day at the start of the line is Tuesday.
+            print(line) #If that statement is truthy, this line prints the line.
 
 
-sales_reports(log_file)
+sales_reports(log_file) #This calls our function with our .txt file as the variable
